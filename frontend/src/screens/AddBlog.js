a42250541d553e92c8bf4a74143df4808281c53e
manojdm <manumanoj709@gmail.com>
2023-08-11 01:33:16 +0530

    feat: added add new blogs page and highlighting texts

--- frontend/src/screens/AddBlog.js
+++ frontend/src/screens/AddBlog.js
@@ -1,24 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addBlog } from "../Actions/blogActions";
 
 const AddBlog = () => {
+	const [title, setTitle] = useState("");
+	const [content, setContent] = useState("");
+
+	const dispatch = useDispatch();
+
+	const handleSubmit = e => {
+		e.preventDefault();
+
+		dispatch(addBlog({ title, content }));
+	};
+
 	return (
 		<div className="add-post-banner">
 			<h2>Add a New Post</h2>
-			<form className="add-post-form">
-				<label for="post-title">Title:</label>
+			<form onSubmit={e => handleSubmit(e)} className="add-post-form">
+				<label htmlFor="post-title">Title:</label>
 				<input
 					type="text"
 					id="post-title"
 					name="post-title"
 					placeholder="Enter post title"
+					value={title}
+					onChange={e => setTitle(e.target.value)}
 					required
 				/>
 
-				<label for="post-content">Content:</label>
+				<label htmlFor="post-content">Content:</label>
 				<textarea
 					id="post-content"
 					name="post-content"
 					placeholder="Enter post content"
+					value={content}
+					onChange={e => setContent(e.target.value)}
 					rows="4"
 					required
 				></textarea>
